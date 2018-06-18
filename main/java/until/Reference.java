0a577136e5801ab3af97e8f6d827c27bc8c130a7
xXboob1Xx <40358569+xXboob1Xx@users.noreply.github.com>
2018-06-18 15:39:47 +0300

    Add files via upload

---
+++ main/java/until/Reference.java
@@ -0,0 +1,10 @@
+package until;
+
+public class Reference
+{
+    public static final String MODID = "tm";
+    public static final String NAME = "mod4eto";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT = "harry.mod.proxy.ClientProxy";
+    public static final String Common = "harry.mod.proxy.CommoProxy";
+}
